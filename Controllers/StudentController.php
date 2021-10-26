16b308aec2441d24e750b4596605e01cd8e12bda
pzaCristian <66428958+pzaCristian@users.noreply.github.com>
2021-10-26 17:33:46 -0300

    Update StudentController.php

--- Controllers/StudentController.php
+++ Controllers/StudentController.php
@@ -4,6 +4,7 @@
      use DAO\StudentDAO as StudentDAO;
      use Models\Student as Student;
      use Utils\Utils as Utils;
+     use DAO\CareerDAO as CareerDAO;
  
      class StudentController
      {
@@ -32,7 +33,28 @@
         public function ShowStudentList($message = "")
         {
             Utils::checkAdminSession();
+            $this->studenList= $this->studentDAO->GetAll();
+            $this->careerList= $this->careerDAO->GetAll();
             require_once(VIEWS_PATH."list-student.php");
+        }
+
+        public function ShowStudent($studentId)
+        {
+            Utils::checkSession();
+            
+            if(isset($_SESSION['admin']) || ($_SESSION['student']->getStudentId() == $studentId)) {
+                $student = $this->studentDAO->GetByStudentId($studentId);
+                $career = $this->careerDAO->getCareerStudent($student);
+    
+                require_once(VIEWS_PATH."student-show.php");
+            }  else {
+                Utils::checkAdminSession();
+            }
+        }
+
+        public function getByEmail($email){
+            $student = $this->studentDAO->GetByStudentEmail($email);
+            return $student;
         }
 
         public function JobOfferManagment($message = "")
