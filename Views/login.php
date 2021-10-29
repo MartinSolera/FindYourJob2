21a98925a5e63580a28c8391fd3768437fa96a9a
pzaCristian <66428958+pzaCristian@users.noreply.github.com>
2021-10-29 17:02:05 -0300

    muestra alumno y completa registro

--- Views/login.php
+++ Views/login.php
@@ -7,15 +7,24 @@
           <form action=<?php echo FRONT_ROOT.'Home/login'?> method="post" class="login-form bg-dark-alpha p-5 text-black">
                <div class="form-group">
                     <label for="">USERNAME</label>
-                    <input type="email" name="email" class="form-control form-control-lg" placeholder="Type email" required>
+                    <input type="email" name="email" class="form-control form-control-lg" placeholder="Type email requerid" required>
                </div>
-             <!--    <div class="form-group">
+               <div class="form-group">
                     <label for="">Password</label>
-                    <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" required>
-               </div>-->
+                    <input type="password" name="password" class="form-control form-control-lg" placeholder="Password requerid" required>
+               </div>
                <button class="btn btn-dark btn-block btn-lg" type="submit" >Log In</button>
+               Opcion 1
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Home/ShowRegister">Registration</a>
             </form>
-            <a class="nav-link" href="<?php echo FRONT_ROOT ?>Home/ShowRegister">Registrarse</a>
+          
+              //Opcion 2
+               <form action="<?php echo FRONT_ROOT ?>Home/ShowRegister" method="get" class="login-form  p-2 bg-none">
+                    <center>
+                         <button class="btn btn-dark" type="submit">Registration</button>
+                    </center>
+               </form>
+
      </div>
 </main>
 
--- Views/user-registration.php
+++ Views/user-registration.php
@@ -1,33 +1,56 @@
-<?php
+<main class="py-5">
+    <section id="listado" class="mb-5">
+        <div class="container">
+            <h2 class="mb-4">Profile</h2>
+            <table class="table bg-light-alpha">
+                <thead>
+                    <th>Name</th>
+                    <th>Last Name</th>
+                    <th>DNI</th>
+                    <th>Gender</th>
+                    <th>Birthday</th>
+                    <th>Id</th>
+                    <th>Career</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                </thead>
+                <tbody>
 
-?>
+                    <?php
+                    if (isset($student)) {
+                        echo  "<td>" . $student->getFirstName() . "</td>";
+                        echo  "<td>" . $student->getLastName() . "</td>";
+                        echo  "<td>" . $student->getDni() . "</td>";
+                        echo  "<td>" . $student->getGender() . "</td>";
+                        echo  "<td>" . $student->getBirthDate() . "</td>";
+                        echo  "<td>" . $student->getFileNumber() . "</td>";
+                        echo  "<td>" . $student->getCareerId() . "</td>";
+                        echo  "<td>" . $student->getEmail() . "</td>";
+                        echo  "<td>" . $student->getPhoneNumber() . "</tdv>";
+                    }
+                    ?>
+                </tbody>
+                </table>
+        </div>
+    </section>
+    <section>
+        <form action='<?php echo FRONT_ROOT ?>Student/studentRegistration' method="post" class="bg-light-alpha p-5">
+            <div>
+                <div class="form-group">
+                    <label for="" align="center"></label>
+                    <input type="email" name="email" class="form-control form-control-lg" placeholder="<?php echo $student->getEmail()?>" required>
+                    <label for="" align="center">Password</label>
+                    <input type="password" alt="strongPass" name="password" class="form-control form-control-lg" placeholder="User required" required>
+                    <label for="" align="center">Confirm Password</label>
+                    <input type="password" alt="strongPass" name="confirmPass" class="form-control form-control-lg" placeholder="Password required" required>
 
-<main class="d-flex align-items-center justify-content-center height-100">
-    <div class="content">
-        <header class="text-center">
-            <h3>Ingrese sus datos:</h3>
-            <h4 style="color: rgb(145, 39, 177)">
-            <?php
-                if (isset($invalidEmail)) {
-                    echo "Error: el email no corresponde a un estudiante activo.";
-                }
-                if (isset($registedEmail)) {
-                    echo "Error: el email ya se encuentra en el sistema.";
-                }
-            ?>
-            </h4>
-        </header>
+                    <center>
+                        <button class="btn btn-warning btn-block btn-sm " type="submit">Registration</button>
+                    </center>
 
-        <form action="<?php echo FRONT_ROOT ?>User/Register" method="POST" class="login-form bg-dark-alpha p-5 bg-light">
-            <div class="form-group">
-                <label for="">Email</label>
-                <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email" required>
+                </div>
+
             </div>
-            <div class="form-group">
-                <label for="">password</label>
-                <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña" required>
-            </div>
-            <button class="btn btn-primary btn-block btn-lg" type="submit">Registrarse</button>
         </form>
-    </div>
+    </section>
 </main>
