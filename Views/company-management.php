88bd7612af1e3ee4b77e959777756fc41e923a84
Solera <msolera@mobydigital.com>
2021-10-31 14:37:48 -0300

    Iconos

--- Views/company-management.php
+++ Views/company-management.php
@@ -2,6 +2,7 @@
     require_once('nav.php');
 ?>
 <main class="py-5">
+<script src="https://kit.fontawesome.com/4fa39b8df5.js" crossorigin="anonymous"></script>
      <section id="listado" class="mb-5">
           <div class="container">
           <h4 style="color:royalblue"><p><?php if(isset($message)){ echo $message; }?></p></h4>
@@ -30,8 +31,9 @@
                         <td><?php echo $company->getEmail(); ?></td>
                         <td><?php echo $company->getPhoneNumber(); ?></td>
                         <td> <img src="<?php if(!empty($company->getLogo())) echo IMG_PATH.$company->getLogo();?>" alt="" width="60" height="30"></td> 
-                        <td><button class="btn btn-danger" ><a href="<?php echo FRONT_ROOT."Company/ShowModifyCompany?idCompany=".$company->getIdCompany();?> " style="color: white;">modify</a></button></td> 
-                        <td><button class="btn btn-danger"><a href="<?php if(isset($company)){echo FRONT_ROOT . "Company/DeleteCompany?idCompany=".$company->getIdCompany();}?> " style="color: white;">delete</a></button></td>      
+                        <td><button class="btn btn-danger" ><a href="<?php echo FRONT_ROOT."Company/ShowModifyCompany?idCompany=".$company->getIdCompany();?> " style="color: white;">Modify </a><i class="fas fa-edit"></i> </button></td> 
+                        
+                        <td><button class="btn btn-danger"><a href="<?php if(isset($company)){echo FRONT_ROOT . "Company/DeleteCompany?idCompany=".$company->getIdCompany();}?> " style="color: white;"> Delete </a><i class="fas fa-trash-alt"></i></button></td>      
                       </tr>
                              <?php }
                           }?>
